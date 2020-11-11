d4c2726955d25632e529d753c01354ca42931367
Simone-Alghisi <alghisius.simone@gmail.com>
2020-11-11 16:50:23 +0100

    Added interaction front-end for user.html

---
+++ public/js/users.js
@@ -0,0 +1,53 @@
+(function ($) {
+  "use strict";
+
+  /**
+   * Mapping of the roles
+   */
+  let roleMapping = {
+    0: "Student*",
+    1: "Professor*",
+    2: "Segretari*"
+  }
+
+  /**
+   * table 
+   */
+  let table = $('#dataTable').DataTable();
+
+  /**
+   * Set the get request to the edit page
+   */
+  function setEventgetUserById(){
+    $('#dataTable tbody').on('click', 'tr', function () {
+      let id = table.row(this).node().id;
+      $(location).prop('href', './editUser.html?id='+id);
+    });
+  }
+
+  /**
+   * Fetch all the users from the RESTFUL api
+   * And load them into the table in the users.html page
+   */
+  function getUsers(){
+    fetch('../api/v1/users')
+    .then((resp) => resp.json())
+    .then(function (data) { 
+      data.map((elem) => {
+        table.row.add([
+          elem.name, 
+          elem.surname, 
+          elem.birth_date, 
+          roleMapping[elem.role]
+        ]).draw().node().id = elem.id;
+      })
+    })
+    .catch(
+      error => console.error(error)
+    );
+  }
+  
+  //Function called
+  getUsers();
+  setEventgetUserById();
+})(jQuery);
