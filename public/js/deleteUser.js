358a1e9ea55d2c6b2a665df995b07b1e10a9d962
Massimo Rizzoli <massimo.rizzoli@studenti.unitn.it>
2020-11-11 17:02:16 +0100

    Added delete integration with UI

---
+++ public/js/deleteUser.js
@@ -0,0 +1,42 @@
+(function ($) {
+  "use strict";
+  
+  let userId = getUrlVars()['id'];
+
+  /**
+   * Function that gets the params in the url
+   */
+  function getUrlVars(){
+    let vars = [], hash;
+    let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++){
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  }
+
+  /**
+   * Function that deletes an user
+   */
+  function deleteUser(){
+    const url = '../api/v1/users/' + userId;
+    let fetchData = {
+      method: 'DELETE'
+    }
+    fetch(url, fetchData)
+      .then((resp) => {
+        console.log(resp)
+        $(location).prop('href', './users.html');
+      })
+      .catch( 
+        error => console.error(error)
+      );
+  }
+  
+  $('#delete').click((event) => {
+    event.preventDefault();
+    deleteUser();
+  });
+})(jQuery);
