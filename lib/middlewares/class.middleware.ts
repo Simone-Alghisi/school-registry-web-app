387e08f46130a87fd208116fc0568e8d0024b262
Simone-Alghisi <alghisius.simone@gmail.com>
2020-11-20 19:22:49 +0100

    added first methods in class's user and middleware

--- lib/middlewares/class.middleware.ts
+++ lib/middlewares/class.middleware.ts
@@ -41,4 +41,65 @@ export class ClassMiddleware {
 			res.status(422).json({ error: 'Unprocessable entity' });
 		}
 	}
+
+	discardUselessFields(req: Request, res: Response, next: NextFunction): void{
+		const classModel = ClassModel.getInstance();
+		try{
+		  const properties: string[] = Object.keys(classModel.classSchema.paths);
+		  for(const key of Object.keys(req.body)){
+			if(properties.indexOf(key) === -1){
+			  delete req.body[key];
+			}
+		  }
+		  next();
+		}catch(e){
+		  res.status(500).send({error: 'Internal server error'});
+		}
+  }
+  
+  async validateClassExists(req: Request, res: Response, next: NextFunction): Promise<void>{
+    const classService = ClassService.getInstance();
+    const classModel = ClassModel.getInstance();
+    let success = false;
+    if(classModel.isValidId(req.params.id)){
+      const user = await classService.getById(req.params.id);
+      if (user) {
+        success = true;
+      } 
+    }
+    if(success){
+      next();
+    }else{
+      res.status(404).json({error: 'Class not found'});
+    }
+  }
+
+  //TODO move it to /lib/middlewares/common.middleware.ts
+  validateUpdateBody(req: Request, res: Response, next: NextFunction): void{
+    if (req.body && Object.keys(req.body).length !== 0) {
+      next();
+    } else {
+      res.status(204).send();
+    }
+  }
+  
+  async validateUpdateRequest(req: Request, res: Response, next: NextFunction): Promise<void>{
+    const updatedClass = await ClassService.getInstance().getById(req.params.id);
+    let valid = false;
+    if(updatedClass){
+      for(const i in req.body){
+        if(ClassMiddleware.validField(req.body[i], '' + i)){
+          console.log('valid: ' + i)
+          valid = true;
+        }else{
+          delete req.body[i];
+        }
+      }
+    }
+    if(valid){
+      next();
+    } else {
+      res.status(204).send();
+    }
+  }
 }
