d1632c8b6c5217d8763158dc78439ed62ae13e89
Massimo Rizzoli <massimo.rizzoli@studenti.unitn.it>
2020-12-09 13:03:54 +0100

    Check communication UI for Professors

---
+++ public/js/communicationsProfessor.js
@@ -0,0 +1,55 @@
+import { refreshToken, dealWithForbiddenErrorCode, dealWithServerErrorCodes, getYourself } from './common.js';
+import { prepareClassOnLoad } from './commonProfessor.js';
+
+(function ($){
+  let table = $('#dataTable').DataTable();
+
+  async function getCommunications(attemptMade=false){
+    let userId = (await getYourself())._id;
+
+    const url = '../api/v1/users/' + userId + '/communications'
+    fetch(url,{
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + window.sessionStorage.accessToken
+      }
+    })
+    .then(resp => {
+      if(resp.ok){
+        return resp.json();
+      }else if(resp.status == 403){
+        if(!attemptMade){
+          refreshToken().then(() => getCommunications(true)).catch(() => dealWithForbiddenErrorCode());
+        }else{
+          dealWithForbiddenErrorCode();
+        }
+      } else {
+        dealWithServerErrorCodes();
+      }
+    })
+    .then(data => {
+      data.map(elem => {
+        table.row.add([
+          elem.subject,
+          getSender(elem)
+        ]).draw().node().id=elem._id;
+      })
+    })
+  }
+
+  function getSender(comm){
+    if(comm.sender_role === 2){
+      return 'segreteria';
+    } else {
+      return comm.sender;
+    }
+  }
+
+  $('#dataTable tbody').on('click', 'tr', function(e){
+    let commId = table.row(this).node().id;
+    $(location).prop('href', './viewCommunicationProfessor.html?id='+commId);
+  });
+
+  prepareClassOnLoad();
+  getCommunications();
+})(jQuery);
---
+++ public/js/viewCommunicationProfessor.js
@@ -0,0 +1,53 @@
+import { refreshToken, dealWithForbiddenErrorCode, dealWithServerErrorCodes, getYourself } from './common.js';
+import { prepareClassOnLoad } from './commonProfessor.js';
+
+(function ($){
+
+  async function loadCommunication(attemptMade=false){
+    const urlParams = new URLSearchParams(location.search);
+    let commId = urlParams.get("id");
+    let userId = (await getYourself())._id;
+
+    if(commId == null){
+      $(location).prop('href', './communicationsProfessor.html');
+    }
+    const url = '../api/v1/users/' + userId + '/communications/' + commId;
+    fetch(url,{
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + window.sessionStorage.accessToken
+      }
+    })
+    .then(resp => {
+      if(resp.ok){
+        return resp.json();
+      }else if(resp.status == 403){
+        if(!attemptMade){
+          refreshToken().then(() => loadCommunication(true)).catch(() => dealWithForbiddenErrorCode());
+        }else{
+          dealWithForbiddenErrorCode();
+        }
+      } else {
+        dealWithServerErrorCodes();
+      }
+    })
+    .then(data => {
+      $('#communication')
+      .append('<div class="col-md-12"><b>Da: </b>' + getSender(data) + '</div><hr>')
+      .append('<div class="col-md-12"><b>Oggetto: </b>' + data.subject + '</div><hr>')
+      .append('<div class="col-md-12"><b>Inviata il: </b>' + 'GG-MM-AAAA' + '</div><hr>')
+      .append('<div class="col-md-12">' + data.content + '</div>');
+    })
+  }
+
+  function getSender(comm){
+    if(comm.sender_role === 2){
+      return 'segreteria';
+    } else {
+      return comm.sender;
+    }
+  }
+
+  prepareClassOnLoad();
+  loadCommunication();
+})(jQuery);
